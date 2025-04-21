f36e10edec5c1e35d0b3c94291c377887054993b
lmyybh <lmyybh_lazy@163.com>
2025-04-21 17:11:40 +0800

    feat(llama3): add generation in llama3

---
+++ llama3/generation.py
@@ -0,0 +1,91 @@
+import torch
+import safetensors.torch
+from collections import OrderedDict
+from modelscope import AutoTokenizer
+
+from .configuration_llama3 import Llama3Config
+from .modeling_llama3 import Llama3
+
+
+class LlamaModel:
+    def __init__(self, tokenizer, model, device="cuda"):
+        self.tokenizer = tokenizer
+        self.model = model
+        self.device = device
+
+    @staticmethod
+    def build_1B(ckpt_file, device="cuda"):
+        config = Llama3Config(
+            vocab_size=128256,
+            num_layers=16,
+            hidden_size=2048,
+            num_attention_heads=32,
+            num_kv_heads=8,
+            intermediate_size=8192,
+            max_seq_len=512,
+            rope_method="transformers",
+            rope_theta=500000.0,
+            rope_scaling=True,
+            rope_scale_factor=32.0,
+            rope_low_freq_factor=1.0,
+            rope_high_freq_factor=4.0,
+            rope_old_context_len=8192,
+        )
+        model = Llama3(config)
+        llama3_dict = LlamaModel._convert_weigths_1B(ckpt_file)
+        model.load_state_dict(llama3_dict)
+        model = model.to(device).eval()
+
+        tokenizer = AutoTokenizer.from_pretrained("LLM-Research/Llama-3.2-1B")
+
+        return LlamaModel(tokenizer, model, device)
+
+    @staticmethod
+    def _convert_weigths_1B(ckpt_file):
+        key_map = {
+            "model.embed_tokens.weight": "token_embedding.lut.weight",
+            "model.norm.weight": "output.norm.gamma",
+            "layers": {
+                "input_layernorm.weight": "attention_norm.gamma",
+                "mlp.down_proj.weight": "ffn.w2.weight",
+                "mlp.gate_proj.weight": "ffn.w3.weight",
+                "mlp.up_proj.weight": "ffn.w1.weight",
+                "post_attention_layernorm.weight": "ffn_norm.gamma",
+                "self_attn.k_proj.weight": "attention.wk.weight",
+                "self_attn.o_proj.weight": "attention.wo.weight",
+                "self_attn.q_proj.weight": "attention.wq.weight",
+                "self_attn.v_proj.weight": "attention.wv.weight",
+            },
+        }
+
+        state_dict = safetensors.torch.load_file(ckpt_file)
+        llama3_dict = OrderedDict()
+
+        for k, v in state_dict.items():
+            if k in key_map:
+                new_key = key_map[k]
+            else:
+                assert "model.layers" in k, f"unknow key: {k}"
+
+                splits = k.split(".")
+                prefix, suffix = ".".join(splits[:3]), ".".join(splits[3:])
+
+                new_key = prefix.replace("model.", "") + "." + key_map["layers"][suffix]
+
+            llama3_dict[new_key] = v.to(torch.float32)
+        llama3_dict["output.linear.weight"] = llama3_dict["token_embedding.lut.weight"]
+
+        return llama3_dict
+
+    @torch.inference_mode()
+    def generate(self, prompt_text, max_length=None):
+        tokens = self.tokenizer.batch_encode_plus([prompt_text], return_tensors="pt")
+        tokens = tokens["input_ids"].to(self.device)
+        output = self.model.generate(
+            tokens, stop_token_id=self.tokenizer.eos_token_id, max_length=max_length
+        )
+        output = self.tokenizer.decode(
+            output, skip_special_tokens=True, clean_up_tokenization_spaces=True
+        )
+
+        return output
